2a7c3dd7741ea4b5c94e19f8dcde154e2f7a085a
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-01-12 19:45:10 +0100

    Running Backup

--- boostSmlPlayground/src/sml_main.cpp
+++ boostSmlPlayground/src/sml_main.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
 #include <iostream>
+#include <typeinfo>
 #include <boost/sml.hpp>
+#include <cxxabi.h>
 
 class BaseDependency
 {
@@ -15,15 +17,6 @@ class ConcreteDependency : public BaseDependency
     void DoStuff() override
     {
         std::cout << "Did stuff" << std::endl;
-    }
-};
-
-class OtherConcreteDependency : public BaseDependency
-{
-  public:
-    void DoStuff() override
-    {
-        std::cout << "Did other stuff" << std::endl;
     }
 };
 
@@ -46,52 +39,92 @@ namespace sml = boost::sml;
 // events
 EVENT(ActivationIntent)
 EVENT(DeactivationIntent)
+EVENT(Active)
+EVENT(Inactive)
 
 // guards
-const auto is_activation_allowed = [](Context& dep, const ActivationIntent&) {
-    dep.a.DoStuff();
-    dep.b.DoStuff();
-    return true;
-};
+const auto is_activation_allowed = [](const ActivationIntent&) { return true; };
 
 // states
 using namespace sml;
-auto init = "initial state"_s;
-auto off = "off state"_s;
-auto on = "on state"_s;
+auto init = sml::state<class initial_state>;
+auto off = sml::state<class off>;
+auto on = sml::state<class on>;
 
 struct OnOffMachine
 {
     auto operator()() const
     {
         using namespace sml;
-        return make_transition_table(*init / [] { std::cout << "Left Init" << std::endl; } = off,
-                                     init + sml::on_exit<_> / []() { std::cout << "Init State exit" << std::endl; },
-                                     off + event<ActivationIntent>[is_activation_allowed] = on,
+        return make_transition_table(*off + event<ActivationIntent>[is_activation_allowed] = on,
                                      off + sml::on_exit<_> / []() { std::cout << "Off State exit" << std::endl; },
-                                     off + sml::on_entry<_> / []() { std::cout << "Off State entry" << std::endl; },
+                                     off +
+                                         sml::on_entry<_> /
+                                             [](Context& ctx) {
+                                                 ctx.a.DoStuff();
+                                                 std::cout << "Off State entry" << std::endl;
+                                             },
                                      on + event<DeactivationIntent> = off,
+                                     on + event<Active> / [] { std::cout << "ActiveAction" << std::endl; } = on,
                                      on + sml::on_exit<_> / []() { std::cout << "On State exit" << std::endl; },
                                      on + sml::on_entry<_> / []() { std::cout << "On State entry" << std::endl; });
     }
 };
 
+template <typename T>
+void GetName(const T& obj)
+{
+    int status;
+    char* realname;
+    const auto& ti = typeid(obj);
+    realname = abi::__cxa_demangle(ti.name(), 0, 0, &status);
+    std::cout << realname << std::endl;
+}
+
+class Visitor
+{
+  public:
+    template <typename TState>
+    // Cannot be specialized to the different types of on/off/init since TState has a different namespace:
+    // boost::sml::v1_1_0::aux <--> boost::sml::v1_1_0::front
+    void operator()(const TState& state) const
+    {
+        std::cout << "Type of state: ";
+        GetName(state);
+        std::cout << "Is in state: " << state.c_str() << std::endl;
+    }
+};
+
+template <>
+void Visitor::operator()(const boost::sml::aux::string<class off>& state) const
+{
+    std::cout << "GOT IT" << std::endl;
+}
+
 int main()
 {
+    GetName(on);
     using namespace sml;
+    Visitor visitor;
 
     ConcreteDependency foo;
-    OtherConcreteDependency Foo;
-    Context ctx{foo, Foo, foo, Foo};
+    Context ctx{foo, foo, foo, foo};
 
     sm<OnOffMachine> sm{ctx};
     assert(sm.is(off));
+    sm.visit_current_states(visitor);
 
     sm.process_event(ActivationIntent{});
     assert(sm.is(on));
+    sm.visit_current_states(visitor);
+
+    sm.process_event(Active{});
+    assert(sm.is(on));
+    sm.visit_current_states(visitor);
 
     sm.process_event(DeactivationIntent{});
     assert(sm.is(off));
+    sm.visit_current_states(visitor);
 
     return 0;
 }
