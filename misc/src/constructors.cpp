42550bfb7fb273eb05878c91cf9b63f36698c687
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-03-15 16:07:08 +0100

    Added lines between sections

--- misc/src/constructors.cpp
+++ misc/src/constructors.cpp
@@ -69,58 +69,76 @@ int main()
 {
     std::cout << "Regular ctor" << std::endl;
     AClass foo{42};  // Default constructor
+    std::cout << '\n';
 
     std::cout << "Copying" << std::endl;
     AClass copy_ctor{foo};  // Copy constructor
+    std::cout << '\n';
 
     std::cout << "Copying" << std::endl;
     AClass also_copy_ctor = foo;  // Also Copy constructor
+    std::cout << '\n';
 
     std::cout << "Copy elision" << std::endl;
-    AClass move_ctor{AClass(42 * 2)};    // Copy elision
+    AClass move_ctor{AClass(42 * 2)};  // Copy elision
+    std::cout << '\n';
+
+    std::cout << "Copy elision" << std::endl;
     AClass move_ctor2 = AClass(42 * 2);  // Copy elision
+    (void)move_ctor2;
+    std::cout << '\n';
+
+    std::cout << "Create function: Assignment" << std::endl;
+    AClass a = CreateAClass();
+    (void)a;
+    std::cout << '\n';
+
+    std::cout << "Create function: Direct init" << std::endl;
+    AClass b{CreateAClass()};
+    std::cout << '\n';
 
     std::cout << "Moving" << std::endl;
     AClass anothother_move_ctor{std::move(AClass(42 * 2))};  // Move constructor
+    std::cout << '\n';
 
     std::cout << "Assignment" << std::endl;
     foo = also_copy_ctor;
+    std::cout << '\n';
 
     std::cout << "Move Assignment" << std::endl;
     foo = AClass(42 * 3);
+    std::cout << '\n';
 
     std::vector<AClass> x;
     std::cout << "Emplace back" << std::endl;
     x.emplace_back(AClass(15));
+    std::cout << '\n';
 
     std::cout << "Push back" << std::endl;
     std::vector<AClass> y;
     y.push_back(AClass(15));
+    std::cout << '\n';
 
     std::cout << "Dynamically resizing a vector" << std::endl;
     std::vector<AClass> z;
     for (int i = 0; i < 10; ++i)
     {
-        std::cout << "Push back #" << i << '\n';
-        std::cout << "Size/Capacity " << z.size() << '/' << z.capacity() << '\n';
+        std::cout << "  Push back #" << i << '\n';
+        std::cout << "    Size/Capacity " << z.size() << '/' << z.capacity() << '\n';
         z.push_back(AClass(16));
     }
+    std::cout << '\n';
 
     std::cout << "Dynamically resizing a vector with reserved elements" << std::endl;
     std::vector<AClass> xy;
     xy.reserve(9);
     for (int i = 0; i < 10; ++i)
     {
-        std::cout << "Push back #" << i << '\n';
-        std::cout << "Size/Capacity " << xy.size() << '/' << xy.capacity() << '\n';
+        std::cout << "  Push back #" << i << '\n';
+        std::cout << "    Size/Capacity " << xy.size() << '/' << xy.capacity() << '\n';
         xy.push_back(AClass(16));
     }
-
-    std::cout << "Create function: Assignment" << std::endl;
-    AClass a = CreateAClass();
-
-    std::cout << "Create function: Direct init" << std::endl;
-    AClass b{CreateAClass()};
+    std::cout << '\n';
 
     return 0;
 }
