42e09a0e6c5fb59c36da31ae152f4b97a4d7ea59
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-03-22 23:21:47 +0100

    Added worker with a different language

---
+++ adapterPatternPlayground/src/foreign_print_worker.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "foreign_print_worker.h"
+
+void ForeignPrintWorker::ArbeiteEtwas()
+{
+    std::cout << "Habe fertig!" << '\n';
+}
---
+++ adapterPatternPlayground/src/foreign_print_worker.h
@@ -0,0 +1,10 @@
+#ifndef FOREIGN_PRINT_WORKER_H
+#define FOREIGN_PRINT_WORKER_H
+
+class ForeignPrintWorker
+{
+  public:
+    void ArbeiteEtwas();
+};
+
+#endif  // FOREIGN_PRINT_WORKER_H
