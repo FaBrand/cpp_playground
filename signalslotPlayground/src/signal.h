bc8494320efdd04118cbe0d65c9b85d44dcde849
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-04-08 14:22:47 +0200

    Added base example for a simple signal slot pattern

--- signalslotPlayground/src/signal.h
+++ signalslotPlayground/src/signal.h
@@ -1,9 +1,28 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
+#include <functional>
+#include <list>
+
 template <typename... Args>
 class Signal
 {
+  public:
+    void connect(std::function<void(Args...)> funclike)
+    {
+        slots_.push_back(funclike);
+    }
+
+    void emit(const Args&... args) const
+    {
+        for (auto const& fun : slots_)
+        {
+            fun(args...);
+        }
+    }
+
+  private:
+    std::list<std::function<void(Args...)>> slots_;
 };
 
 #endif /* SIGNAL_H */
--- signalslotPlayground/src/signal_slot.cpp
+++ signalslotPlayground/src/signal_slot.cpp
@@ -1,5 +1,16 @@
 #include "signal.h"
+#include <cstring>
+#include <iostream>
+
 int main()
 {
+    Signal<> signal;
+    signal.connect([]() { std::cout << "Normal Lambda" << '\n'; });
+    signal.emit();
+
+    Signal<std::string> string_signal;
+    string_signal.connect([](const auto& text) { std::cout << "Received Text:" << text << '\n'; });
+    string_signal.emit("abc");
+
     return 0;
 }
