5cc7332deeedf3d4495b33a8adb3745c66055140
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-08-01 23:09:57 +0200

    Add basic headers for essential concepts

---
+++ MVCplayground/include/mvc/controller.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+class Controller
+{
+public:
+    Controller();
+    virtual ~Controller();
+};
+
+#endif /* CONTROLLER_H */
---
+++ MVCplayground/include/mvc/view.h
@@ -0,0 +1,11 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+class View
+{
+public:
+    View();
+    virtual ~View();
+};
+
+#endif /* VIEW_H */
