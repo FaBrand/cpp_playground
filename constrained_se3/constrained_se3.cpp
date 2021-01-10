0df8344efea6f6e67ec5bb5b75afd9eeeb632475
Andrei Herdt <andrei.herdt@tra.ai>
2021-01-10 22:46:38 +0000

    update constrained_se3

--- constrained_se3/constrained_se3.cpp
+++ constrained_se3/constrained_se3.cpp
@@ -13,59 +13,60 @@ struct Control {};
 
 template <typename GoalFrame, typename OriginFrame>
 struct SO3 {
-    SO3(double value) { value_ = value; };
+    SO3(double value) { data = value; };
 
-    double value_{0};
+    double data{0};
     GoalFrame goal_frame_;
     OriginFrame origin_frame_;
 };
 
 template <typename GoalFrame, typename OriginFrame>
 struct SE3 {
-    SE3(double value) { value_ = value; };
+    SE3(double value) { data = value; };
 
-    double value_{0};
+    double data{0};
     GoalFrame goal_frame_;
     OriginFrame origin_frame_;
 };
 
 template <typename Frame>
-struct Position {
-    Position(double value) { value_ = value; };
-
-    template <typename GoalFrame>
-    void Rotate(const SO3<GoalFrame, Frame>& rot){
-        value_ += rot.value_;
-        frame_ = GoalFrame{};
-    }
+struct Euclidian {
+    Euclidian(double value) { data = value; };
 
     std::variant<World, Base, Control> frame_;
-    double value_{0};
+    double data{0};
 };
 
-template <typename T1, typename T2>
-Position<T1> operator*(const SE3<T1, T2>& se3, const Position<T2>& pos) {
+template <typename GoalFrame, typename Frame>
+void Rotate(Euclidian<Frame>& vec, const SO3<GoalFrame, Frame>& rot){
+    vec.data += rot.data;
+    vec.frame_ = GoalFrame{};
+}
 
-    Position<T1> new_pos{.value_ = se3.value_*pos.value_};
+template <typename T1, typename T2>
+Euclidian<T1> operator*(const SE3<T1, T2>& se3, const Euclidian<T2>& pos) {
+
+    Euclidian<T1> new_pos{.data = se3.data*pos.data};
     return new_pos;
 }
 
 template <typename T1, typename T2, typename T3>
 SE3<T1, T3> operator*(const SE3<T1, T2>& M1, const SE3<T2, T3> M2) {
-    SE3<T1, T3> new_se3{.value_ = M1.value_*M2.value_};
+    SE3<T1, T3> new_se3{.data = M1.data*M2.data};
     return new_se3;
 }
 
 int main() {
     SE3<World, Base> w_T_b(1);
-    SO3<Control, Base> c_R_b(1);
-    SE3<Base, Foot> b_T_f(3);
-    Position<Base> p_b(2);
+    Euclidian<Base> p_b(2);
+    std::cout << (w_T_b * p_b).data << std::endl;
 
-    std::cout << (w_T_b * p_b).value_ << std::endl;
-    std::cout << (w_T_b * b_T_f).value_ << std::endl;
-    p_b.Rotate(c_R_b);
-    std::cout << p_b.value_ << std::endl;
+    SE3<Base, Foot> b_T_f(3);
+    std::cout << (w_T_b * b_T_f).data << std::endl;
+
+    SO3<Control, Base> yaw_d_R_b(4);
+    Rotate(p_b, yaw_d_R_b);
+    std::cout << p_b.data << std::endl;
 
     return 0;
 }
