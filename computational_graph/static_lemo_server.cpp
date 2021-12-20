20a335c9e9448ebca43257fa4da745f0f001084e
Andrei Herdt <andrei.herdt@tra.ai>
2021-12-20 15:47:26 +0000

    add notes.md

--- computational_graph/static_lemo_server.cpp
+++ computational_graph/static_lemo_server.cpp
@@ -1,6 +1,20 @@
 // TODO:
 // - write base class
 
-class MotorControllerNode {};
+class MotorControllerNode {
+    BusSignals bus_signals;
+    Node position_wrapped;
+    Node position;
+    Node velocity;
+    Node vel_filt;
+    Node joint_position;
+};
 
-int main() { MotorControllerNode mc_node; }
+int main() {
+    MotorControllerNode mc_node;
+
+    bus_signals.motor_position() >> motor_position_wrapped >> motor_position >>
+        motor_velocity >> motor_vel_filt;
+    motor_position >> motor_output_position;
+    mc_node_joint_position() >> position;
+}
---
+++ notes.md
@@ -0,0 +1,5 @@
+Comp graph
+- outline usage
+- create core component connection.h 
+https://github.com/FlexCoreLib/flexcore/blob/master/docs/CoreConcept.md<Paste>
+https://github.com/FlexCoreLib/flexcore/blob/master/flexcore/core/connection.hpp
