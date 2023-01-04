b5b8e6c1d3f79cd133febecc806b50a8dd93b9e1
Andrei Herdt <herdt@LP-L0247.local>
2023-01-04 21:58:07 +0000

    add KKT equations

--- proxqp/test.cpp
+++ proxqp/test.cpp
@@ -26,9 +26,40 @@ auto computeGradientPDAL(const Eigen::MatrixXd& H,  //
                          const Eigen::MatrixXd& A,  //
                          const Eigen::VectorXd& b   //
                          ) -> Eigen::VectorXd {
+    // Set up KKT matrix
+    Eigen::MatrixXd KKT =
+        Eigen::MatrixXd::Zero(H.rows() + A.rows(), H.cols() + A.rows());
+    Eigen::VectorXd v = Eigen::VectorXd::Zero(H.rows() + A.cols());
+
+    Eigen::MatrixXd I1 = Eigen::MatrixXd::Identity(H.rows(), H.cols());
+    Eigen::MatrixXd I2 = Eigen::MatrixXd::Identity(A.rows(), A.rows());
+
+    constexpr double rho = 0.1;
+    constexpr double mu_e = 0.1;
+
+    // UL
+    KKT.block(0, 0, H.rows(), H.cols()) = H + rho * I1;
+    // LL
+    KKT.block(H.rows(), 0, A.rows(), A.cols()) = A;
+    // UR
+    KKT.block(0, H.cols(), A.cols(), A.rows()) = A.transpose();
+    // LR
+    KKT.block(H.rows(), H.cols(), A.rows(), A.rows()) = -mu_e * I2;
     // TODO: Build KKT matrix and factorize. It is not yet clear how the matrix
     // is being built from the model.
     // TODO: Get solve function from ldl
+
+    Eigen::VectorXd xk = Eigen::VectorXd::Zero(H.rows());
+    Eigen::VectorXd yk = Eigen::VectorXd::Zero(A.rows());
+
+    v.head(H.rows()) = rho * xk - g;
+    v.tail(A.rows()) = b - mu_e * yk;
+    std::cout << "KKT:" << std::endl;
+    std::cout << KKT << std::endl;
+    std::cout << "v:" << std::endl;
+    std::cout << v << std::endl;
+
+    return xk;
 }
 
 TEST_CASE("Simple Least Squares", "[ProxQP]") {
