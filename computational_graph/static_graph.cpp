71bcb94b6fb02f32520ef0d8311153fe91f5e286
Andrei Herdt <herdt@arrival.com>
2023-03-19 15:28:15 +0000

    minor

--- computational_graph/static_graph.cpp
+++ computational_graph/static_graph.cpp
@@ -1,13 +1,11 @@
-// TODO:
 
-class MotorControllerNode {
-    BusSignals bus_signals;
-    Node position_wrapped;
-    Node position;
-    Node velocity;
-    Node vel_filt;
-    Node joint_position;
+class BusSignal {
+    std::string node_name = "";
 };
+
+class Node {
+    std::string node_name = "";
+}
 
 // cfg.read("scale", scale_);
 // cfg.read("offset", offset_);
@@ -15,11 +13,18 @@ class MotorControllerNode {
 //     value_ = (input_->getValue() - offset_) * scale_ + bias_;
 // }
 
-int main() {
-    MotorControllerNode mc_node;
+static
+operator>>(const Node& input) {
+    std::cout << "input.node_name:" << input.node_name << std::endl;
+}
 
-    bus_signals.motor_position >> motor_position_wrapped >> motor_position >>
+int main() {
+    BusSignal motor_position;
+    Node motor_position_wrapped;
+    Node motor_position;
+    Node motor_velocity;
+    Node motor_vel_filt;
+
+    motor_position >> motor_position_wrapped >> motor_position >>
         motor_velocity >> motor_vel_filt;
-    motor_position >> motor_output_position;
-    mc_node_joint_position() >> position;
 }
--- computational_graph/todo.md
+++ computational_graph/todo.md
@@ -1,1 +1,2 @@
+- [ ] Specify 
 - [ ] Make compile
